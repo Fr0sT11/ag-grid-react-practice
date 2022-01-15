7788cb207ddb6feb8b61e45f5ecd692c3cfe7312
Sumit <sumit.sth54@gmail.com>
2022-01-15 13:43:09 +0545

    Added sortable,filterable and selecteable grid

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useRef } from 'react';
+import { AgGridColumn, AgGridReact } from 'ag-grid-react';
 
-function App() {
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+
+const App = () => {
+  const [rowData, setRowData] = useState([]);
+  const gridRef = useRef(null);
+
+  useEffect(() => {
+    fetch('https://www.ag-grid.com/example-assets/row-data.json')
+    .then(result => result.json())
+    .then(rowData => setRowData(rowData))
+  }, [])
+
+  const onButtonClick = e => {
+    const selectedNodes = gridRef.current.api.getSelectedNodes();
+    const selectedData = selectedNodes.map( node => node.data );
+    const selectedDataStringPresentation = selectedData.map( node => `${node.make} ${node.model}`).join(', ')
+    alert(`Selected nodes: ${selectedDataStringPresentation}`)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="ag-theme-alpine"
+      style={{height:400, width:700}}  
+    >
+      <button onClick={onButtonClick}>Get Selected Rows</button>
+      <AgGridReact
+        rowData={rowData}
+        rowSelection="multiple">
+          <AgGridColumn field="make" sortable={ true } filter={ true } checkboxSelection={true}></AgGridColumn>
+          <AgGridColumn field="model" sortable={ true } filter={ true }></AgGridColumn>
+          <AgGridColumn field="price" sortable={ true } filter={ true }></AgGridColumn>
+        </AgGridReact>
+
     </div>
   );
 }
